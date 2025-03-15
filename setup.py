29338e59c3cf14e3d41c6babf065ce55e48df009
sromero1992 <selimiles52@gmail.com>
2025-03-15 14:43:22 -0500

    Pipelines installable and callable anywhere, jupyter notebook has example of usage

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-DESCRIPTION = "A collection of functions for Visium HD data processing",
+DESCRIPTION = "A collection of functions for Visium HD data processing"
 
 # Find packages inside the 'qfeatures' folder
 PACKAGES = find_packages(where="visium_hd_functions")
@@ -39,13 +39,11 @@ setup(
     description=DESCRIPTION,
     author='Selim Romero',
     author_email="ssromerogon@tamu.edu",  # Replace with your email
-    license="MIT"
+    license="MIT",
     url="https://github.com/sromero1992/visium_hd_processing",  # URL for your GitHub repo or package
-    packages=PACKAGES,  # Look for packages inside the 'qfeatures' folder                                                                                                                       
-    keywords=KEYWORDS,                                                                                                                                                                          
-    package_dir={'visium_hd_functions': 'visium_hd_functions'},  # Adjust to match your structure                                                                                                                   
-    install_requires=INSTALL_REQUIRES,  # Add dependencies                                                                                                                                      
-    python_requires='>=3.9,<3.10',  # Specify Python version compatibility for any 3.9.x version                                                                                                
-    description="A collection of functions for Visium HD data processing",
+    packages=PACKAGES,  # Look for packages inside the 'qfeatures' folder
+    keywords=KEYWORDS,
+    package_dir={'visium_hd_functions': 'visium_hd_functions'},  # Adjust to match your structure
+    install_requires=INSTALL_REQUIRES,  # Add dependencies
+    python_requires='>=3.9,<3.10',  # Specify Python version compatibility for any 3.9.x version
 )
-
